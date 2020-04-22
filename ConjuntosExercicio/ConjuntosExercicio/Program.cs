80d4410687ae68a0bfa14452f716d3d372aaa98d
iannleles <51231213+iannleles@users.noreply.github.com>
2020-04-21 21:19:13 -0300

    Add projetos

---
+++ ConjuntosExercicio/ConjuntosExercicio/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConjuntosExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+            HashSet<int> C = new HashSet<int>();
+  
+            Console.Write("O curso A possui quantos alunos? ");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os codigos dos alunos do curso A: ");
+            for (int i=0; i<N; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                A.Add(x);
+            }
+
+
+            Console.Write("O curso B possui quantos alunos? ");
+            N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os codigos dos alunos do curso B: ");
+            for (int i = 0; i < N; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                B.Add(x);
+            }
+
+            Console.Write("O curso C possui quantos alunos? ");
+            N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os codigos dos alunos do curso C: ");
+            for (int i = 0; i < N; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                C.Add(x);
+            }
+
+            HashSet<int> Novo = new HashSet<int>();
+
+            Novo.UnionWith(A);
+            Novo.UnionWith(B);
+            Novo.UnionWith(C);
+            Console.WriteLine("Total de alunos: " + Novo.Count);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Conta conta;
+            Console.Write("Entre com o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Havera deposito inicial : (s/n)? ");
+
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre com o valor do deposito Inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(numero, titular, depositoInicial);
+                        
+            }
+            else
+            {
+                conta = new Conta(numero, titular);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para deposito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+            Console.WriteLine();
+            Console.Write("Entre com os dados do saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+            
+
+        }
+    }
+}
