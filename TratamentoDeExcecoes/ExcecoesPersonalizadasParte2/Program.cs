e8e9525987e9695c2788adcad3aa9ce398beb478
Iann Leles <iannleles@outlook.com>
2020-04-25 14:11:28 -0300

    Add Tratamento de Excecoes

---
+++ TratamentoDeExcecoes/ExcecoesPersonalizadasParte2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ExcecoesPersonalizadasParte2.Entities;
+
+namespace ExcecoesPersonalizadasParte2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in");
+            }
+            else
+            {
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data top update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                string error = reservation.UpdateDates(checkIn, checkOut);
+
+                if (error != null)
+                {
+                    Console.WriteLine("Error in reservation: " + error);
+                }
+                
+                else
+                {
+                    reservation.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+            }
+        }
+    }
+}
